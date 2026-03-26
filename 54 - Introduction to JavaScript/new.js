9826b460547603453ead3f42fffaada3b40689a3
ankiiitttt <ankitdewangan225@gmail.com>
2026-03-26 14:40:08 +0530

    introduction to js

---
+++ 54 - Introduction to JavaScript/new.js
@@ -0,0 +1,4 @@
+console.log("Hello World")
+console.log("the code is running---0")
+console.log(" the code is running ---1")
+console.log(" the code is running---2")
---
+++ 54 - Introduction to JavaScript/script.js
@@ -0,0 +1,19 @@
+alert("hello world")
+
+
+console.log("Hey i am console")
+console.log("the code is running---0")
+console.log(" the code is running ---1")
+console.log(" the code is running---2")
+
+var a = prompt("enter your number")
+console.log("Your number is " + a)
+
+var isTrue = confirm("Sure you want to leave this site?")
+
+if(isTrue){
+    console.log("site is closing");
+}
+else{
+    console.log("not closing");
+}
